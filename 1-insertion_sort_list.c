4946d282da4b67f83896965cd6e9e5f0f1554f2c
Mthokozisi Zwane <mthokozisi.zwane1999@gmail.com>
2023-09-24 17:41:29 -0700

    insert_sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -38,6 +38,7 @@ void insertion_sort_list(listint_t **list)
 
 			prev->prev = current;
 			prev = current->prev;
+			print_list(*list);
 		}
 
 		current = temp; /* Moving to the next node for the next iteration*/
